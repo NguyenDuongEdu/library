32a66622276b1fe46f45815cfc9f3b2c9609755a
NguyenDuongEdu <141114727+NguyenDuongEdu@users.noreply.github.com>
2025-01-20 10:57:14 +0700

    Add files via upload

---
+++ source/js/script4.js
@@ -0,0 +1,47 @@
+var elementorFrontendConfig = {
+    "environmentMode": {
+        "edit": false,
+        "wpPreview": true
+    },
+    "i18n": {
+        "shareOnFacebook": "Chia s\u1ebb tr\u00ean facebook",
+        "shareOnTwitter": "Chia s\u1ebd tr\u00ean Twitter",
+        "pinIt": "Ghim n\u00f3",
+        "downloadImage": "T\u1ea3i h\u00ecnh \u1ea3nh"
+    },
+    "is_rtl": false,
+    "breakpoints": {
+        "xs": 0,
+        "sm": 480,
+        "md": 768,
+        "lg": 1025,
+        "xl": 1440,
+        "xxl": 1600
+    },
+    "version": "2.9.12",
+    "urls": {
+        "assets": "https:\/\/builder.simplepage.vn\/wp-content\/plugins\/elementor\/assets\/"
+    },
+    "settings": {
+        "page": [],
+        "general": {
+            "elementor_global_image_lightbox": "yes",
+            "elementor_lightbox_enable_counter": "yes",
+            "elementor_lightbox_enable_fullscreen": "yes",
+            "elementor_lightbox_enable_zoom": "yes",
+            "elementor_lightbox_enable_share": "yes",
+            "elementor_lightbox_title_src": "title",
+            "elementor_lightbox_description_src": "description"
+        },
+        "editorPreferences": []
+    },
+    "post": {
+        "id": 2076991,
+        "title": "abc%20%E2%80%93%20SimplePage",
+        "excerpt": "",
+        "featuredImage": false
+    },
+    "user": {
+        "roles": ["author"]
+    }
+};
